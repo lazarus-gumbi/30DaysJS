0cf6e71b8e58535014ec831a10f87cbdff7c4ac1
Melusi Lazarus Gumbi <gumbimelusi2@gmail.com>
2022-07-09 15:21:12 +0200

    Day 1 and Two

---
+++ intro.js
@@ -0,0 +1,10 @@
+let nums = '1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125'
+let rd = Math.random()*11
+let str = "love is love"
+let user = {
+    'name':'Melusi',
+    'job':'Software Engineer'
+
+}
+
+console.log(nums)
